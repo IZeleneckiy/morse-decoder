c560f9e4c0b427f2b4ad07590990a28bb1eea1a7
Ivan Zeleneckiy <izeleneckiy@gmail.com>
2020-10-02 10:09:42 +0400

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+"use strict"
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -38,9 +39,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    while (expr.length % 10) {
+        expr = '0' + expr;
+    }
+    let array = [];
+    MORSE_TABLE['**********'] = ' ';
+    for (let i = 0; i < expr.length; i += 10) {
+        array.push(expr.substring(i, i + 10));
+    }
+    array = array.map(a => a.replace(/11/g, '-')).map(a => a.replace(/10/g, '.')).map(a => a.replace(/[00]/g, ''));
+    for (let i = 0; i < array.length; i++) {
+        array[i] = MORSE_TABLE[array[i]];
+    }
+    return array.join('');
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('00000000100000111010101010111100111011100000001011111110101011111010101010101010'));
